0efc2bf681f02780b924cba5f28157df33ebb9be
Majid L <majidl@peoplestrust.com>
2026-08-07 16:29:24 +0200

    add remote-state project

---
+++ trailmark/state-backend/main.tf
@@ -0,0 +1,25 @@
+### Create an S3 bucket to host the frontend files
+resource "aws_s3_bucket" "state_backend" {
+  bucket = var.bucket
+}
+
+resource "aws_s3_bucket_versioning" "state_backend_versioning" {
+  bucket = aws_s3_bucket.state_backend.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "state_backend_public_access_block" {
+  bucket = aws_s3_bucket.state_backend.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.state_backend.bucket
+  description = "Output for bucket name"
+}
---
+++ trailmark/state-backend/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "bucket" {
+  type    = string
+  default = "trailmark-state-backend"
+}
